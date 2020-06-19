c34db828838b340d0208f8e080b0b6ff89b4719c
Zeralyos <kellbyb@gmail.com>
2020-06-19 00:05:37 -0400

    Added setup() and changeFuelStart()

--- gatorTest.js
+++ gatorTest.js
@@ -4,27 +4,26 @@ var hze = 0;
 var ticked = false;
 var fuelStart = 230;
 var fuelEnd = 230;
-var fuelZones = fuelEnd - fuelStart;
+var fuelZones = 0;
 var runEnd = 230;
 var housingMod = 1;
 var spiresCleared = 0;
-var carp = 0;
-var carp2 = 0;
-var carpMod = 0;
-var coord = 0;
-var randimp = false;
+var carpentry = 0;
+var carpentry2 = 0;
+var carpenryMod = 0;
+var coordinated = 0;
 var tauntimpFrequency = 2.97;
 var efficiency = 0;
+var efficiencyMod = 1
 var capacity = 0;
 var maxCapacity = 3;
 var supply = 0;
 var maxSupply = 0.2;
-var overclock = 0;
 var overclocker = 0;
-var storage = 2;
-var slowburn = 0.4;
-var magmaFlow = true;
-var magmaCells = 18;
+var overclockRate = 0;
+var storage = 1;
+var fuelPerTick = 0.5;
+var magmaCellsPerZone = 16;
 var popPerTick = 0;
 var minTick = 0;
 var maxTick = 0;
@@ -39,18 +38,19 @@ var totalPop = 0;
 var currentPop = [];
 var finalAmals = 0;
 var tauntimpPercent = 0;
-var maxAmals = 0;
+var maxAmalgamators = 0;
 var lastCoord = 0;
-var finalAmalZone = 0;
+var finalAmalgamatorZone = 0;
 var neededPop = 0;
 var finalArmySize = 0;
-var coordIncrease = 0;
-var coordinations = [];
+var coordinationArmyIncrease = 0;
+var ArmySizes = [];
+var coordinationThresholds;
 var finalAmalRatio = 0;
 var yourFinalRatio = 0;
-var zonesOfMI = 0;
 var magmaZones = 0;
-var totalMI = 0;
+var magmiteZones = 0;
+var totalMagmite = 0;
 
 var ar1 = 10000000000;
 var ar2;
@@ -58,9 +58,9 @@ var ar3;
 var ar4;
 var ar5;
 
-var uncoords = 0;
-var uncoordsZone = -1;
-var uncoordsGoal = 1;
+var withheldCoords = 0;
+var withholdStopZone = -1;
+var totalGatorGoal = 1;
 var fuelThisZone = [];
 var totalFuel = [];
 var overclockTicks = [];
@@ -84,7 +84,18 @@ function setup() {
 	document.getElementById("version").innerHTML = version;
 }
 
-function changeFuelStart() {}
+function changeFuelStart(value) {
+	fuelStart = Math.max(230, parseInt(value));
+	document.getElementById("fuelStart").value = fuelStart
+	if (fuelEnd < fuelStart) {
+		fuelEnd = fuelStart;
+		document.getElementbyId("fuelEnd").value = fuelEnd;
+	}
+	if (runEnd < fuelStart) {
+		runEnd = fuelStart;
+		document.getElementById("runEnd").value = runEnd;
+	}
+}
 
 function changeFuelEnd() {}
 
